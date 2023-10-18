2338dea35ec5c2e6d02c3832e4523057d07148dd
Kevin Madrigal <kv.madrigal07@gmail.com>
2023-10-17 20:35:05 -0600

    Classes Final Commit

---
+++ CKK.Logic/Models/Product.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    public class Product
+    {
+        private int _id;
+        private string _name;
+        private decimal _price;
+
+        // GetId and SetId for _id field
+        public int GetId()
+        {
+            return _id;
+        }
+        public void SetId(int id)
+        {
+            _id = id;
+        }
+
+        // GetName and SetName for _name field
+        public string GetName()
+        {
+            return _name;
+        }
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+
+        // GetPrice and SetPrice for _price field
+        public decimal GetPrice()
+        {
+            return _price;
+        }
+        public void SetPrice(decimal price)
+        {
+            _price = price;
+        }
+    }
+}
---
+++ CKK.Logic/Models/ShoppingCartItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    internal class ShoppingCartItem
+    {
+        private Product _product;
+        private int _quantity;
+
+        // SoppingCartItem cunstroctor for shopping cart items
+        public ShoppingCartItem(Product product, int quantity)
+        {
+            _product = product;
+            _quantity = quantity;
+        }
+
+        // GetQuantity set and get methods
+        public int GetQuantity()
+        {
+            return _quantity;
+        }
+        public void SetQuantity(int quantity)
+        {
+            _quantity = quantity;
+        }
+
+        // Product get and set methods
+        public Product GetProduct()
+        {
+            return _product;
+        }
+        public void SetProduct(Product product)
+        {
+            _product = product;
+        }
+    }
+}
