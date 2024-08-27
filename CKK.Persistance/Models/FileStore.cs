7825828043718f09b2242091d9c480e74e4c4ba6
madrigal1102 <kv.madrigal07@gmail.com>
2024-08-26 20:42:15 -0600

    added CKK.Persistance

---
+++ CKK.Persistance/Models/FileStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using CKK.Logic.Interfaces;
+using CKK.Logic.Models;
+
+
+
+namespace CKK.Persistance.Models
+{
+    public class FileStore : IStore, ILoadable, ISavable
+    {
+
+    }
+}
